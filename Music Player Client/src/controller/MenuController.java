90820fc7057ae6d2d5f247413df367812ddaeb5f
Alexander G Bonasera <alexbonasera6@gmail.com>
2017-03-08 22:28:24 -0500

    Remake Connect Input Dialog.

--- Music Player Client/src/controller/MenuController.java
+++ Music Player Client/src/controller/MenuController.java
@@ -1,17 +1,12 @@
 package controller;
 
-import javafx.event.ActionEvent;
+import java.util.Optional;
+
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import model.SongModel;
 import view.MenuView;
 import view.PrimaryView;
@@ -27,37 +22,25 @@ public class MenuController {
 		menuView.onClickEvent(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
+				
+				TextInputDialog connectDialog = new TextInputDialog();
+				ButtonType connectButtonType = new ButtonType("Connect", ButtonData.OK_DONE);
+				
+				connectDialog.getDialogPane().getButtonTypes().set(0, connectButtonType);
+				connectDialog.getDialogPane().setHeaderText("Enter the IP you wish to connect to.");
+				connectDialog.getDialogPane().setContentText("IP:");
+				
+				Optional<String> result = connectDialog.showAndWait();
+				if (result.isPresent()) {
+					try {
+						SongModel songModel = tcpCtrl.requestSongs(result.get());
+						treeCtrl.updateArtists(songModel);
+						tableCtrl.updateSongs(songModel);
+						statusCtrl.updateCount(songModel);
+					} catch (NullPointerException e) {
 
-				Stage stage = new Stage();
-				Label ipLabel = new Label("IP: ");
-				TextField ipField = new TextField();
-				HBox hb = new HBox();
-				hb.setAlignment(Pos.CENTER);
-				hb.getChildren().addAll(ipLabel, ipField);
-				VBox vb = new VBox(10);
-				vb.setPadding(new Insets(10, 0, 0, 0));
-				Button connectButton = new Button("Connect");
-
-				connectButton.setOnAction(new EventHandler<ActionEvent>() {
-
-					@Override
-					public void handle(ActionEvent event) {
-						try {
-							SongModel songModel = tcpCtrl.requestSongs(ipField.getText());
-							treeCtrl.updateArtists(songModel);
-							tableCtrl.updateSongs(songModel);
-							statusCtrl.updateCount(songModel);
-						} catch (NullPointerException e) {
-
-						} finally {
-							stage.close();
-						}
 					}
-				});
-
-				vb.getChildren().addAll(hb, connectButton);
-				stage.setScene(new Scene(vb, 200, 100));
-				stage.showAndWait();
+				}
 			};
 		});
 

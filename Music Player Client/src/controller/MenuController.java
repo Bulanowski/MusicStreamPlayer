8803b565c1b0f3698ec8522e150f228423167360
Alexander G Bonasera <alexbonasera6@gmail.com>
2017-03-11 18:58:44 -0500

    MenuController Fix

--- Music Player Client/src/controller/MenuController.java
+++ Music Player Client/src/controller/MenuController.java
@@ -8,32 +8,38 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
 import model.SongModel;
+import model.TCP;
+import model.TCPSongDAO;
+import model.TableFilter;
 import view.MenuView;
 import view.PrimaryView;
+import view.SearchChangedEvent;
+import view.SearchChangedListener;
 
 public class MenuController {
+
 	private MenuView menuView;
 
-	public MenuController(PrimaryView primaryView, TCPController tcpCtrl, TreeController treeCtrl,
-			TableController tableCtrl, StatusController statusCtrl) {
+	public MenuController(PrimaryView primaryView, TreeController treeCtrl, TableController tableCtrl,
+			StatusController statusCtrl, ChatController chatBoxCtrl, TCP tcp) {
 
 		menuView = new MenuView();
 
-		menuView.onClickEvent(new EventHandler<MouseEvent>() {
+		menuView.onConnectClickEvent(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				
+
 				TextInputDialog connectDialog = new TextInputDialog();
 				ButtonType connectButtonType = new ButtonType("Connect", ButtonData.OK_DONE);
-				
-				connectDialog.getDialogPane().getButtonTypes().set(0, connectButtonType);
+
 				connectDialog.getDialogPane().setHeaderText("Enter the IP you wish to connect to.");
 				connectDialog.getDialogPane().setContentText("IP:");
-				
+				connectDialog.getDialogPane().getButtonTypes().set(0, connectButtonType);
+
 				Optional<String> result = connectDialog.showAndWait();
 				if (result.isPresent()) {
 					try {
-						SongModel songModel = tcpCtrl.requestSongs(result.get());
+						SongModel songModel = new SongModel(new TCPSongDAO(tcp, result.get()));
 						treeCtrl.updateArtists(songModel);
 						tableCtrl.updateSongs(songModel);
 						statusCtrl.updateCount(songModel);
@@ -42,6 +48,24 @@ public class MenuController {
 					}
 				}
 			};
+		});
+
+		menuView.onChatClickEvent(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				chatBoxCtrl.show();
+			}
+
+		});
+
+		menuView.setSearchChangedListener(new SearchChangedListener() {
+
+			@Override
+			public void searchChanged(SearchChangedEvent event) {
+				tableCtrl.applyFilter(event.getSearchText(), TableFilter.ALL);
+			}
+
 		});
 
 		primaryView.setTop(menuView.getNode());

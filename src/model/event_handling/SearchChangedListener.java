b361dea203b06ad6599be3cd55c3987fdb8b888a
Alexander G Bonasera <alexbonasera6@gmail.com>
2017-03-27 17:30:10 -0400

    Delete SearchChangedListener.java

--- src/model/event_handling/SearchChangedListener.java
+++
@@ -1,9 +0,0 @@
-package model.event_handling;
-
-import java.util.EventListener;
-
-public interface SearchChangedListener extends EventListener {
-
-	public void searchChanged(SearchChangedEvent event);
-
-}
